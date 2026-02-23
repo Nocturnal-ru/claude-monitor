a380115ede272620d7f2f5d9ad383f2f8bf600b0
nocturnal_red <42446434+Nocturnal-ru@users.noreply.github.com>
2026-02-23 11:59:25 +0300

    Add files via upload

---
+++ icons.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "embed"
+)
+
+//go:embed icons/green.ico
+var iconGreen []byte
+
+//go:embed icons/yellow.ico
+var iconYellow []byte
+
+//go:embed icons/red.ico
+var iconRed []byte
+
+//go:embed icons/gray.ico
+var iconGray []byte
